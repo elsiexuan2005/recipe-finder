6529c3fff4fe398dc7722e8ae148d6d81327e792
MacBook <macbook@MacBook-Pro-cua-MacBook.local>
2024-08-29 21:13:30 +0700

    Modify recipe cards display, add boder lines and paddings

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -5,32 +5,35 @@ const RecipeCard = ({ recipe }) => {
 
   return (
 
-    <div className='recipe'>
-      <h2>{title}</h2>
+    <div className='recipe' style= {{width: '20rem', margin: 'auto',  border: '2px solid black', borderRadius: '8px', padding: '10px'}}>
       <img src={image} alt={title}/>
-      <div className='used-ingredients'>
-        <h3>Used ingredients</h3>
-        <ul>
-            {usedIngredients.map((ingredients, index)=> (
-                <li key={index}>
-                    <img src={ingredients.image} alt={ingredients.name} />
-                    {ingredients.original}
-                </li>
-            )
-            )}
-        </ul>
-      </div>
-      <div className='missing-ingredients'>
-      <ul>
-            {missedIngredients.map((ingredients, index)=> (
-                <li key={index}>
-                    <img src={ingredients.image} alt={ingredients.name} />
-                    {ingredients.original}
-                </li>
-            )
-            )}
-        </ul>
-      </div>
+      <div class="card-body">
+        <h2 className='card-title'>{title}</h2>
+            <div className='used-ingredients'>
+                <h3>Used ingredients</h3>
+                <ul>
+                    {usedIngredients.map((ingredients, index)=> (
+                        <li key={index}>
+                            
+                            {ingredients.original}
+                        </li>
+                    )
+                    )}
+                </ul>
+            </div>
+            <div className='missing-ingredients'>
+            <h3>Missing ingredients</h3>
+            <ul>
+                    {missedIngredients.map((ingredients, index)=> (
+                        <li key={index}>
+                        
+                            {ingredients.original}
+                        </li>
+                    )
+                    )}
+                </ul>
+            </div>
+        </div>
     </div>
 
   );
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,16 +3,22 @@ import RecipeCard from "./RecipeCard";
 
 const RecipeList = ({recipes}) => {
     return (
-        <div>
+      <div id="recipeCarousel" className="carousel slide" data-bs-ride="carousel">  
+        <div className='recipe-list'>
           {recipes.length > 0 ? (
             recipes.map((recipe, index) => (
-              <RecipeCard key={index} recipe={recipe} />
+              <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={recipe.id}>
+                <RecipeCard key={index} recipe={recipe} />
+              </div>  
             ))
           ) : (
-            <p>No recipes found</p>
+            <div className='carousel-item active'>
+              <p>No recipes found</p>
+            </div>  
           )}
         </div>
+      </div>  
       );
     };
-   
+
 export default RecipeList
